7c957f73d340c39ce49f341d61d2205a143fd2cb
tommycwu <tommycwu@yahoo.com>
2020-04-16 13:47:30 -0700

    Initial commit

---
+++ Start.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Start.aspx.cs" Inherits="PerfTest.Start" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #TextArea1 {
+            height: 315px;
+            width: 761px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <br />
+        api url&nbsp;
+        <asp:TextBox ID="TextBoxUrl" runat="server" Width="724px">https://company.oktapreview.com/api/v1/authn</asp:TextBox>
+        <br />
+        <br />
+        username
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        password
+        <asp:TextBox ID="TextBox4" runat="server" TextMode="Password">1_Thousand!!!!!</asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="ButtonPost" runat="server" OnClick="ButtonPost_Click" Text="POST" />
+&nbsp;<asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Loop" />
+        <asp:TextBox ID="TextBox2" runat="server">50</asp:TextBox>
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server" Height="71px" TextMode="MultiLine" Width="770px"></asp:TextBox>
+        <br />
+        <br />
+        <asp:ListBox ID="ListBox1" runat="server" Height="254px" Width="388px"></asp:ListBox>
+        <asp:ListBox ID="ListBox2" runat="server" Height="254px" Width="388px"></asp:ListBox>
+        <br />
+        <br />
+        <asp:Label ID="LabelTotal" runat="server" Text="Total Time: 0"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Start.aspx.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PerfTest
+{
+    public partial class Start : System.Web.UI.Page
+    {
+        public string PostAuthN(string apiUrl, string userName, string userPassword)
+        {
+            string responseText = "";
+            try
+            {
+                Uri endpoint = new Uri(apiUrl);
+                HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(endpoint);
+                if (webRequest != null)
+                {
+                    webRequest.Method = "POST";
+                    webRequest.Accept = "application/json";
+                    webRequest.ContentType = "application/json";
+                    string postData = "{\"username\": \"" + userName + "\",\"password\": \"" + userPassword + "\",\"options\": {\"multiOptionalFactorEnroll\": true,\"warnBeforePasswordExpired\": true}}";
+                    byte[] byteMe = Encoding.UTF8.GetBytes(postData);
+                    webRequest.ContentLength = byteMe.Length;
+                    Stream requestStream = webRequest.GetRequestStream();
+                    requestStream.Write(byteMe, 0, byteMe.Length);
+                    HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+                    Stream responseStream = response.GetResponseStream();
+                    StreamReader readStream = new StreamReader(responseStream, Encoding.UTF8);
+                    string jsonString = readStream.ReadToEnd();
+                    dynamic jsonObject = JsonConvert.DeserializeObject(jsonString);
+                    JToken parsedJson = JToken.Parse(jsonObject.ToString());
+                    responseText = parsedJson.ToString(Formatting.Indented);
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return responseText;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonPost_Click(object sender, EventArgs e)
+        {
+            ListBox1.Items.Add(DateTime.UtcNow.ToString("hh:mm:ss:ffff"));
+            TextBox1.Text = "Running... ";
+            TextBox1.Text = PostAuthN(TextBoxUrl.Text, TextBox3.Text, TextBox4.Text);
+            ListBox2.Items.Add(DateTime.UtcNow.ToString("hh:mm:ss:ffff"));
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string countString = TextBox2.Text;
+            int countInt = Int32.Parse(countString);
+            DateTime StartDT = DateTime.UtcNow;
+            for (int i = 0; i < countInt; i++)
+            {
+                ButtonPost_Click(ButtonPost, null);
+            }
+            DateTime StopDT = DateTime.UtcNow;
+            TimeSpan TotalTime = StopDT - StartDT;
+            LabelTotal.Text = "Total time = " + TotalTime.TotalMilliseconds.ToString();
+        }
+    }
+}
